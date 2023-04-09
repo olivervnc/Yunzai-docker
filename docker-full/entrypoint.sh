0c2389e250742e6c1f891c5546ae5a04bfc723ae
olivervnc <92519287+olivervnc@users.noreply.github.com>
2023-04-09 02:43:05 +0100

    f

--- docker-full/entrypoint.sh
+++ docker-full/entrypoint.sh
@@ -5,6 +5,11 @@ if [[ ! -d config ]] || [[ -z `ls -A config` ]]; then
     cp ${DEFAULT}/config ./ -r
 fi
 
+if [[ ! -d ${HOME} ]] || [[ -z `ls -A ${HOME}` ]]; then
+    echo "copying default home folder"
+    cp ${DEFAULT}/home/* ${HOME} -r
+fi
+
 cp ${DEFAULT}/plugins ./ -r
 
 pnpm install -P
