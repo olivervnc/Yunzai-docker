86d8cb292ddd9f7dd97543255ec5a05f9c6e2daf
olivervnc <92519287+olivervnc@users.noreply.github.com>
2023-04-09 05:28:45 +0100

    b

--- docker-full/install.sh
+++ docker-full/install.sh
@@ -6,10 +6,9 @@ git clone --depth=1 -b main https://gitee.com/yoimiya-kokomi/Yunzai-Bot.git ${YU
 cd ${YUNZAI} 
 git clone --depth=1 https://github.com/yoimiya-kokomi/miao-plugin.git ./plugins/miao-plugin/
 
-if [ -f "./config/config/redis.yaml" ]; then
-    sed -i 's/127.0.0.1/redis/g' ./config/config/redis.yaml
-    echo -e "\n  修改Redis地址完成~  \n"
-fi
+cp ./config/default_config/redis.yaml ./config/config
+sed -i 's/127.0.0.1/redis/g' ./config/config/redis.yaml
+echo -e "\n  修改Redis地址完成~  \n"
 
 pnpm install -P
 mv config plugins ${DEFAULT}
