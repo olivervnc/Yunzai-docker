4a10d087df199bab84c0b15211f03afe93e8811d
olivervnc <92519287+olivervnc@users.noreply.github.com>
2023-04-09 05:11:18 +0100

    ss

--- docker-full/install.sh
+++ docker-full/install.sh
@@ -8,8 +8,8 @@ git clone --depth=1 https://github.com/yoimiya-kokomi/miao-plugin.git ./plugins/
 pnpm install -P
 
 if [ -f "./config/config/redis.yaml" ]; then
-    sed -i 's/127.0.0.1/redis/g' ./config/config/redis.yaml && \
-    echo -e "\n  修改Redis地址完成~  \n" || true
+    sed -i 's/127.0.0.1/redis/g' ./config/config/redis.yaml
+    echo -e "\n  修改Redis地址完成~  \n"
 fi
 
 mv config plugins ${DEFAULT}
